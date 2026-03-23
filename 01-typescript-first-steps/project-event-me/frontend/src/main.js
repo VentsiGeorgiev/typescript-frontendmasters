d0d15c2d1b9d49cec10adc41bdabb53b08e059a8
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-23 22:24:11 +0100

    format main js file

--- 01-typescript-first-steps/project-event-me/frontend/src/main.js
+++ 01-typescript-first-steps/project-event-me/frontend/src/main.js
@@ -1,26 +1,21 @@
-import './style.css'
-import Header, { setupThemeToggle } from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import { setupModals } from './components/Modal';
-import { setupForms } from './components/Forms';
-
+import "./style.css";
+import Header, { setupThemeToggle } from "./components/Header.js";
+import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
+import { setupModals } from "./components/Modal.js";
+import { setupForms } from "./components/Forms.js";
 
 // Quick and dirty - not for production!
 const render = (html) => {
-  const app = document.querySelector('#app');
+  const app = document.querySelector("#app");
   app.innerHTML = html;
   setupThemeToggle();
   setupModals();
   setupForms();
-}
-
+};
 
 render(`
   ${Header}
   ${Main}
   ${Footer}
 `);
-
-
-
