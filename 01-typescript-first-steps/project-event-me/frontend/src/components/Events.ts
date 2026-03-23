7a18afb7c6c42ab4b891844ddb1baab34d2701c9
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-23 22:24:34 +0100

    convert events js file to typescript

--- 01-typescript-first-steps/project-event-me/frontend/src/components/Events.ts
+++ 01-typescript-first-steps/project-event-me/frontend/src/components/Events.ts
@@ -2,7 +2,30 @@ import { Calendar } from "./Icons.js";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const loadEventsData = async () => {
+type Host = {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+};
+type Rsvps = {
+  event_id: number;
+  name: string;
+  email: string;
+};
+
+type Event = {
+  id: number;
+  title: string;
+  description: string;
+  image_url: string;
+  date: Date;
+  host_id: number;
+  host: Host;
+  rsvps: Rsvps[];
+};
+
+const loadEventsData = async (): Promise<Event[] | undefined> => {
   try {
     const response = await fetch(`${API_URL}/events`);
     return response.json();
@@ -11,8 +34,8 @@ const loadEventsData = async () => {
   }
 };
 
-export const EventModal = (event) => {
-  const formId = `rsvp-form-${event.ID}`;
+export const EventModal = (event: Event) => {
+  const formId = `rsvp-form-${event.id}`;
   const modalId = `modal-event-${event.id}`;
   return `<dialog id="${modalId}">
       <article>
@@ -51,9 +74,10 @@ export const EventModal = (event) => {
     </dialog>`;
 };
 
-export const EventCard = (e) => {
+export const EventCard = (e: Event) => {
   const eventDate = new Date(e.date);
   const isPast = eventDate < new Date();
+  const description = e.description ? `<p>${e.description}</p>` : "";
   return `
 <article class="event" >
 <header>
@@ -63,8 +87,7 @@ export const EventCard = (e) => {
         <h4>${e.title}</h4>
         <p>${Calendar} ${eventDate.toLocaleDateString()}</p>
         <p>Host: ${e.host?.name || `User ${e.host_id}`}</p>
-
-        ${e.description && `<p>${e.description}</p>`}
+        ${description}
     </main>
     <footer>
         <span>
@@ -86,7 +109,7 @@ export const EventCard = (e) => {
     `;
 };
 
-export const EventsSection = (title, events) => {
+export const EventsSection = (title: string, events: Event[]) => {
   return `
   <section class='events'>
       <h2>${title} events </h2>
@@ -100,6 +123,7 @@ export const EventsSection = (title, events) => {
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 export const Events = await (async () => {
   const all = await loadEventsData();
+  if (!all) return;
   const past = all.filter((e) => new Date(e.date) < new Date());
   const upcoming = all.filter((e) => new Date(e.date) > new Date());
   return `
