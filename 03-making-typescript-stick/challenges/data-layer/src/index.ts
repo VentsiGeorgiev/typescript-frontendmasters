9eef4973160f93c55a7afd4fbe9b8b0e91e6a1bb
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-04-18 10:40:58 +0200

    implement typed DataStore for movies and songs

--- 03-making-typescript-stick/challenges/data-layer/src/index.ts
+++ 03-making-typescript-stick/challenges/data-layer/src/index.ts
@@ -8,9 +8,75 @@ export interface Song extends DataEntity {
   singer: string;
 }
 
+// export interface Comic extends DataEntity {
+//   issueNumber: number;
+// }
+
 export type DataEntityMap = {
   movie: Movie;
   song: Song;
+  // comic: Comic;
 };
 
-export class DataStore {}
+type DataStoreMethods = {
+  [K in keyof DataEntityMap as `getAll${Capitalize<K>}s`]: () => DataEntityMap[K][];
+} & {
+  [K in keyof DataEntityMap as `get${Capitalize<K>}`]: (
+    id: string,
+  ) => DataEntityMap[K];
+} & {
+  [K in keyof DataEntityMap as `clear${Capitalize<K>}s`]: () => void;
+} & {
+  [K in keyof DataEntityMap as `add${Capitalize<K>}`]: (
+    arg: DataEntityMap[K],
+  ) => DataEntityMap[K];
+};
+
+function isDefined<T>(x: T | undefined): x is T {
+  return typeof x !== "undefined";
+}
+
+export class DataStore implements DataStoreMethods {
+  #data: { [K in keyof DataEntityMap]: Record<string, DataEntityMap[K]> } = {
+    movie: {},
+    song: {},
+  };
+  getAllSongs(): Song[] {
+    return Object.keys(this.#data.song)
+      .map((songKey) => this.#data.song[songKey])
+      .filter(isDefined);
+  }
+  getSong(songKey: string): Song {
+    const song = this.#data.song[songKey];
+    if (!song) throw new Error(`Could not find song with id ${songKey}`);
+    return song;
+  }
+  addSong(s: Song): Song {
+    this.#data.song[s.id] = s;
+    return s;
+  }
+  clearSongs(): void {
+    this.#data.song = {};
+  }
+
+  getAllMovies(): Movie[] {
+    return Object.keys(this.#data.movie)
+      .map((movieKey) => this.#data.movie[movieKey])
+      .filter(isDefined);
+  }
+  getMovie(movieKey: string): Movie {
+    const movie = this.#data.movie[movieKey];
+    if (!movie) throw new Error(`Could not find movie with id ${movieKey}`);
+    return movie;
+  }
+  addMovie(m: Movie): Movie {
+    this.#data.movie[m.id] = m;
+    return m;
+  }
+  clearMovies(): void {
+    this.#data.movie = {};
+  }
+}
+
+// const ds: DataStoreMethods = {} as any;
+// ds.
